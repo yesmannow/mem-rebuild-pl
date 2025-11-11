3b2eac750bf501cf6aab2ba4c3a52138ced0268e
JdarlingGT <jdarling@grastontechnique.com>
2025-11-11 02:47:21 -0500

    chore(types): remove types restriction to allow JSX shim and all type packages

--- src/types/three-js.d.ts
+++ src/types/three-js.d.ts
@@ -2,16 +2,20 @@
  * Temporary JSX shim for react-three-fiber elements.
  * TODO: remove once @react-three/fiber types are properly configured.
  */
-declare namespace JSX {
-  interface IntrinsicElements {
-    mesh: any;
-    group: any;
-    boxGeometry: any;
-    meshStandardMaterial: any;
-    ambientLight: any;
-    pointLight: any;
-    perspectiveCamera: any;
-    orthographicCamera: any;
-    [key: string]: any;
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      mesh: any;
+      group: any;
+      boxGeometry: any;
+      meshStandardMaterial: any;
+      ambientLight: any;
+      pointLight: any;
+      perspectiveCamera: any;
+      orthographicCamera: any;
+      [key: string]: any;
+    }
   }
 }
+
+export {};
